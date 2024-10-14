a9b2c3aba421700c62584cafb2a46e6b47ef7f48
Cuyeshi Yunkay <cuyeshi@mail.ru>
2024-10-14 22:53:05 +0300

    Добавьте файлы проекта.

---
+++ Models/Genre.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RPBDIS_lab2.Models;
+
+public partial class Genre
+{
+    public int GenreId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public virtual ICollection<Book> Books { get; set; } = new List<Book>();
+}
